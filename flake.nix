3982f606ff9644bcf49aabe87e7254427d25ebb4
pspiagicw <pspiagicw@gmail.com>
2024-10-07 08:28:46 +0000

    feat: added support for aarch64 flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
-  description = "Neovim configuration with stylua";
+  description = "Go development environment";
 
   inputs = {
     # Nixpkgs repository
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-
     custom.url = "github:pspiagicw/packages";
   };
 
@@ -17,13 +16,23 @@
       pkgs = import nixpkgs {system = "x86_64-linux";};
     in
       pkgs.mkShell {
-        hardeningDisable = ["fortify"];
         buildInputs = [
-          pkgs.go
           pkgs.gopls
           pkgs.delve
           pkgs.gotools
-          custom.packages.x86_64-linux.groom
+          pkgs.go
+        ];
+      };
+    devShell.aarch64-linux = let
+      pkgs = import nixpkgs {system = "aarch64-linux";};
+    in
+      pkgs.mkShell {
+        buildInputs = [
+          pkgs.gopls
+          pkgs.delve
+          pkgs.gotools
+          pkgs.go
+          custom.packages.aarch64-linux.groom
         ];
       };
   };
